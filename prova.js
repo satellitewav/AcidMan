c5486a5e1d7e9c5e4de01c23703ed1bc6291cbbd
JackDiSpade21 <alearci44@gmail.com>
2022-10-13 22:03:07 +0200

    TEST INTERNO 2

--- prova.js
+++ prova.js
@@ -1,9 +1,15 @@
-
-
 function suona(){
     const audioCtx = new AudioContext();
     const audio = new Audio("/pacman-firebase/audio/pause_beat.mp3");
     const source = audioCtx.createMediaElementSource(audio);
     source.connect(audioCtx.destination);
     audio.play();
+}
+
+function due(){
+    const audioCtx = new AudioContext();
+    const suono = new Audio("/pacman-firebase/audio/game_start.mp3");
+    const source = audioCtx.createMediaElementSource(suono);
+    source.connect(audioCtx.destination);
+    suono.play();
 }
