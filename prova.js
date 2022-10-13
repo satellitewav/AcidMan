2ed6a5eaae9f5b1a12efb93cd724d5fcd59888bf
JackDiSpade21 <alearci44@gmail.com>
2022-10-13 19:12:36 +0200

    AAAAAAA

--- prova.js
+++ prova.js
@@ -2,7 +2,7 @@
 
 function suona(){
     const audioCtx = new AudioContext();
-    const audio = new Audio("/audio/pause_beat.mp3");
+    const audio = new Audio("/pacman-firebase/audio/pause_beat.mp3");
     const source = audioCtx.createMediaElementSource(audio);
     source.connect(audioCtx.destination);
     audio.play();
